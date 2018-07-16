df9c03410b45a4c6e6d924b7df96268c0f112438
usamit02 <usamit02@gmail.com>
2018-07-16 09:58:33 +0900

    データベース試験

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
   <HEAD>
     <META HTTP-EQUIV='Content-Type' CONTENT='text/html;charset=UTF-8'>
     <meta name="viewport" content="width=640">
-    <title>記事</title>
+    <title>記事一覧</title>
     <link rel="stylesheet" href="css/index.css">
 
   </HEAD>
@@ -15,6 +15,8 @@ session_start();
   <BODY>
     <?php
 require_once(__DIR__."/include/dbinit.php");
+$na=$db->query("SELECT id,na FROM t01user WHERE id=1;")->fetchcolumn();
+echo$na;
 ?>
 
   </BODY>
